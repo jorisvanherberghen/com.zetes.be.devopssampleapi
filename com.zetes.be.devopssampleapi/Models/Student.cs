664793394aa3ecfd1dc5fa2cabc47b4fff92c7ae
jorisvanherberghen <joris.vanherberghen@be.zetes.com>
2018-10-17 14:52:58 +0200

    Added student model and controller

---
+++ com.zetes.be.devopssampleapi/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace com.zetes.be.devopssampleapi.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
